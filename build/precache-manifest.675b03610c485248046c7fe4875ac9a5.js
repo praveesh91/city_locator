8e54e3df2bcf5b0913aa06a73cbd2176da5b1195
praveesh91 <praveesh91@example.com>
2020-07-21 21:47:29 +0530

    build done

---
+++ build/precache-manifest.675b03610c485248046c7fe4875ac9a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77527d4bd7632883543385183f36585e",
+    "url": "/city_locator/index.html"
+  },
+  {
+    "revision": "08a9aba1b9432af3b13c",
+    "url": "/city_locator/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3f7e892638a817daab7b",
+    "url": "/city_locator/static/css/main.b3d26884.chunk.css"
+  },
+  {
+    "revision": "08a9aba1b9432af3b13c",
+    "url": "/city_locator/static/js/2.adfd1bc1.chunk.js"
+  },
+  {
+    "revision": "a90a0449d8ac6943f0cd62c0929c2a63",
+    "url": "/city_locator/static/js/2.adfd1bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7e892638a817daab7b",
+    "url": "/city_locator/static/js/main.3a3bdd58.chunk.js"
+  },
+  {
+    "revision": "e46d225414cde3a52b2a",
+    "url": "/city_locator/static/js/runtime-main.f41622c9.js"
+  }
+]);
